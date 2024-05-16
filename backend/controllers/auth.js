f9dc86dd7d1566dec0f63c7584f21fd46afc5e3d
Saul Muñoz <sam.pedreros14@gmail.com>
2024-05-16 14:15:22 -0400

    chore: Update auth.js to handle user registration and password hashing

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,30 @@
-function register(request, response) {
-  console.log("Se ha ejecutado el registros");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
-  response.status(200).send({ msg: "Todo OK" });
+async function register(request, response) {
+  const { firstname, lastname, email, password } = request.body;
+
+  if (!email) response.status(400).send({ msg: "El email es obligatorio" });
+  if (!password) response.status(400).send({ msg: "La contraseña es obligatoria" });
+
+  const user = new User({
+    firstname,
+    lastname,
+    email: email.toLowerCase(),
+    role: "user",
+    active: false,
+  });
+
+  const salt = bcrypt.genSaltSync(10);
+  const hashpassword = bcrypt.hashSync(password, salt);
+  user.password = hashpassword;
+
+  try {
+    const userStorage = await user.save();
+    response.status(200).send({ user: userStorage });
+  } catch (error) {
+    response.status(400).send({ msg: "Error al guardar el usuario" });
+  }
 }
 
-module.exports = { register, };
+module.exports = { register };
