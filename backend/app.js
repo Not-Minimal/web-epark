389d002731f1595c3bac0372eca61eeae75d4c4d
Saul Muñoz <sam.pedreros14@gmail.com>
2024-05-16 13:33:21 -0400

    Crear Servicor HTTP con express

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+//Importar Express
+const express = require('express');
+const { API_VERSION } = require('./constants');
+
+// Inicializar Express
+const app = express();
+
+// import routings
+// ...
+
+// Configure Body Parse
+// ...
+
+// Configure HTTP headers - CORS
+
+// Configure routings
+// ...
+
+// exportar Express
+module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
-const { DB_USER, DB_PASSWORD, DB_HOST } = require('./constants');
+const app = require('./app');
+const { DB_USER, DB_PASSWORD, DB_HOST, IP_SERVER, API_VERSION } = require('./constants');
+
+const PORT = process.env.POST || 3977;
 
 mongoose.connect(
   `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/?retryWrites=true&w=majority&appName=web-epark`
 )
   .then(() => {
-    console.log("La conexion con la base de datos ha sido exitosa");
+    app.listen(PORT, () => { // Corrected 'port' to 'PORT'
+      console.log("#############################################");
+      console.log("##### API RESTful de ePark de EPIS #########");
+      console.log(`http://${IP_SERVER}:${PORT}/api/${API_VERSION}/`);
+    });
   })
   .catch((error) => {
     console.error("Error al conectar con la base de datos:", error);
