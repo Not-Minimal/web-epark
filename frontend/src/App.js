a6d147eee1c9de966438f51eac1cd9c50dc6afd6
Saul Muñoz <sam.pedreros14@gmail.com>
2024-05-16 12:13:12 -0400

    chore: Update App.js to toggle car state on button click usando useState

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,34 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Saludar from './components/Saludar';
+// import Saludar from './components/Saludar';
 
 function App() {
 
 
-  const user = {
-    nombre: "Saul",
-    edad: 24,
-    color: "Azul"
-  };
+  // const user = {
+  //   nombre: "Saul",
+  //   edad: 24,
+  //   color: "Azul"
+  // };
 
-  const saludarFn = (nombre, edad) => {
-    console.log("Hola " + nombre + " tiene " + edad + " años");
-    console.log(`Hola ${nombre} tiene ${edad} años`);
+  // const saludarFn = (nombre, edad) => {
+  //   console.log("Hola " + nombre + " tiene " + edad + " años");
+  //   console.log(`Hola ${nombre} tiene ${edad} años`);
+  // };
+  const [stateCar, setStateCar] = useState(false); // [valor, funcion
+  const encenderApagar = () => {
+    // setStateCar(!stateCar);
+    setStateCar(prevValue => !prevValue)
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Saludar userInfo={user} saludarFn={saludarFn} />
+        <h3>El coche esta: {stateCar ? "Encendido" : "Apagado "}</h3>
+        <button onClick={encenderApagar}>Encencender / Apagar</button>
+        {/* <Saludar userInfo={user} saludarFn={saludarFn} /> */}
       </header>
     </div>
   );
