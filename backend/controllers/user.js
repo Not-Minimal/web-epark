72ba5cf70d31b1e1e433a2758d6a6dfe38436300
Saul Muñoz <sam.pedreros14@gmail.com>
2024-05-17 01:12:44 -0400

    chore: Add connect-multiparty dependency for handling file uploads in createUser endpoint

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,29 +1,59 @@
 const User = require('../models/user');
-
+const bcrypt = require("bcryptjs");
 
 async function getMe(req, res) {
   const { user_id } = req.user;
 
-  const response = await User.findById(user_id);
-  if (!response) {
-    return res.status(400).send({ msg: 'Usuario no encontrado' });
-  } else {
-    res.status(200).send({ response });
+  try {
+    const response = await User.findById(user_id);
+    if (!response) {
+      return res.status(400).send({ msg: 'Usuario no encontrado' });
+    } else {
+      res.status(200).send({ response });
+    }
+  } catch (error) {
+    res.status(500).send({ msg: 'Error del servidor' });
   }
 }
 
 async function getUsers(req, res) {
-  //Obtener Query Params
   const { active } = req.query;
   let response = null;
 
-  if (active === undefined) {
-    response = await User.find();
-  } else {
-    response = await User.find({ active });
+  try {
+    if (active === undefined) {
+      response = await User.find();
+    } else {
+      response = await User.find({ active });
+    }
+    res.status(200).send({ response });
+  } catch (error) {
+    res.status(500).send({ msg: 'Error del servidor' });
   }
-  res.status(200).send({ response });
-
 }
 
-module.exports = { getMe, getUsers };
+async function createUser(req, res) {
+  const { password } = req.body;
+  const user = new User({
+    ...req.body,
+    active: false,
+  });
+
+  const salt = bcrypt.genSaltSync(10);
+  const hashPassword = bcrypt.hashSync(password, salt);
+  user.password = hashPassword;
+
+  if (req.files && req.files.avatar) {
+    // TODO: Process Avatar
+    console.log("Procesar Avatar");
+  }
+
+  try {
+    const userStored = await user.save();
+    res.status(200).send({ user: userStored });
+  } catch (error) {
+    res.status(500).send({ msg: "Error del servidor" });
+  }
+}
+
+module.exports = { getMe, getUsers, createUser };
