18447f19a7bfcdb264ed4d9dae6792b385ad2d08
Saul Muñoz <sam.pedreros14@gmail.com>
2024-05-17 00:36:11 -0400

    chore: Update getMe endpoint to fetch user data from the database

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,13 @@
-async function getMe(request, response) {
-  response.status(200).send({ msg: 'OK' });
+const User = require('../models/user');
+async function getMe(req, res) {
+  const { user_id } = req.user;
+
+  const response = await User.findById(user_id);
+  if (!response) {
+    return res.status(400).send({ msg: 'Usuario no encontrado' });
+  } else {
+    res.status(200).send({ response });
+  }
 }
 
 module.exports = { getMe };
