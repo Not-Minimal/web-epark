36873cc218f29a3bfb460ea166f7d053010fc182
Saul Muñoz <sam.pedreros14@gmail.com>
2024-05-18 13:19:41 -0400

    chore: Add createMenu endpoint to create a new menu

--- backend/controllers/menu.js
+++ backend/controllers/menu.js
@@ -1,4 +1,22 @@
 const Menu = require('../models/menu');
 
+/**
+ * Crea un nuevo menú y lo guarda en la base de datos.
+ *
+ * @param {Object} req - El objeto de solicitud HTTP.
+ * @param {Object} res - El objeto de respuesta HTTP.
+ */
+async function createMenu(req, res) {
+  const menu = new Menu(req.body);
 
-module.exports = {};
+  try {
+    const menuStored = await menu.save();
+    res.status(200).send({ menu: menuStored });
+  } catch (error) {
+    res.status(400).send({ message: 'Error al crear menú.' });
+  }
+}
+
+module.exports = {
+  createMenu,
+};
--- backend/router/menu.js
+++ backend/router/menu.js
@@ -5,4 +5,6 @@ const md_auth = require('../middlewares/authenticated');
 const api = express.Router();
 
 // ENDPOINTS
+api.post("/menu", [md_auth.asureAuth], MenuController.createMenu);
+
 module.exports = api;
