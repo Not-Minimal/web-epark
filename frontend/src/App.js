2448910a348c8f551a4448bdff8453b3f6f4175f
Saul Muñoz <sam.pedreros14@gmail.com>
2024-05-20 18:18:29 -0400

    chore: Update package.json and index.js, and remove unused files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import Saludar from './components/Saludar';
+import React from 'react'
 
-function App() {
-
-
-  // const user = {
-  //   nombre: "Saul",
-  //   edad: 24,
-  //   color: "Azul"
-  // };
-
-  // const saludarFn = (nombre, edad) => {
-  //   console.log("Hola " + nombre + " tiene " + edad + " años");
-  //   console.log(`Hola ${nombre} tiene ${edad} años`);
-  // };
-  const [stateCar, setStateCar] = useState(false); // [valor, funcion
-  const [contar, setContar] = useState(0);
-
-  useEffect(() => {
-    console.log("Total: " + contar);
-  }, [contar]);
-
-  const encenderApagar = () => {
-    setStateCar(!stateCar);
-    setContar(contar + 1);
-    // setStateCar(prevValue => !prevValue)
-  };
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>El coche esta: {stateCar ? "Encendido" : "Apagado "}</h3>
-        <h4>Clicks: {contar}</h4>
-        <button onClick={encenderApagar}>Encencender / Apagar</button>
-        {/* <Saludar userInfo={user} saludarFn={saludarFn} /> */}
-      </header>
+    <div>
+      <h1 className='text-3xl font-bold underline'>Hola</h1>
     </div>
-  );
+  )
 }
-
-export default App;
