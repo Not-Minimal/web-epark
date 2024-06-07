a862810f47a8008f72452ecc227b30964920a064
Saul Muñoz <sam.pedreros14@gmail.com>
2024-06-06 22:17:01 -0400

    chore: Update routes and components for user creation, vehicle creation, and dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,17 +5,16 @@ import "./index.css";
 import Navigation from "./components/Navigation.jsx";
 import CreateUser from "./components/CreateUser.jsx";
 import CreateVehicle from "./components/CreateVehicle.jsx";
-import Orders from "./components/Orders.jsx";
+import Dashboard from "./components/Dashboard";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Navigation />
       <Routes>
-        <Route path="/" exact element={<CreateUser />} />
+        <Route path="/" exact element={<Dashboard />} />
         <Route path="/create" element={<CreateUser />} />
         <Route path="/create/vehicle" element={<CreateVehicle />} />
-        <Route path="/orders" element={<Orders />} />
       </Routes>
     </Router>
   </React.StrictMode>
