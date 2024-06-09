377df7dbf95d36f4fe348be804205a6ebb970bbe
Saul Muñoz <sam.pedreros14@gmail.com>
2024-06-09 01:10:23 -0400

    chore: Update landing page with centered layout and improve header styling

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,11 +1,10 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
-
 export default function Landing() {
   return (
     <div className="flex flex-col items-center min-h-[100dvh]">
-      <header className="flex items-center gap-2 p-8 lg:px-6">
-        <div className="flex items-center gap-2">
+      <header className="flex items-center gap-2 p-8 lg:px-6 ">
+        <a className="flex items-center gap-2" rel="ugc">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -24,27 +23,31 @@ export default function Landing() {
             <path d="M19 9a7 7 0 1 0-13.6 2.3C6.4 14.4 8 19 8 19h8s1.6-4.6 2.6-7.7c.3-.8.4-1.5.4-2.3"></path>
             <path d="M12 19v3"></path>
           </svg>
-          <Link to="/" className="text-xl font-bold">
+          <Link to="/" className="text-xl font-bold ">
             E-Park
           </Link>
-        </div>
+        </a>
       </header>
       <main className="flex-1">
         <section id="Hero2" className="bg-white dark:bg-black">
+          {" "}
           <div className="flex flex-col justify-between px-8 mx-auto max-w-screen-xl text-center lg:py-16 lg:flex-row">
+            {" "}
             <div className="mb-4 lg:mb-0 lg:pt-32 lg:max-w-lg lg:pr-5">
+              {" "}
               <div>
+                {" "}
                 <h1 className="lg:text-7xl first:mx-auto m-6 max-w-2xl font-display text-5xl font-medium tracking-tight text-slate-900 dark:text-white sm:text-7xl">
                   Gestiona tu Estacionamiento con
                   <span className="relative whitespace-nowrap text-green-400">
-                    <span className="relative"> E-Park.</span>
-                  </span>
-                </h1>
+                    <span className="relative"> E-Park.</span>{" "}
+                  </span>{" "}
+                </h1>{" "}
                 <p className="mx-auto m-12 mt-16 max-w-2xl text-xl tracking-tight text-slate-900 dark:text-slate-100">
                   Descubra el futuro del aparcamiento con e-park, nuestra nueva
                   aplicacion de estacioamiento en linea y seguro.
-                </p>
-              </div>
+                </p>{" "}
+              </div>{" "}
               <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                 <Link
                   to="/login"
@@ -58,19 +61,19 @@ export default function Landing() {
                 >
                   Crear Cuenta
                 </Link>
-              </div>
-            </div>
+              </div>{" "}
+            </div>{" "}
             <div>
               <img
                 src="/src/assets/Dashboard.png"
                 className="object-cover object-top w-full h-64 mx-auto lg:h-auto xl:mr-24 md:max-w-sm"
                 alt=""
               ></img>
-            </div>
-          </div>
+            </div>{" "}
+          </div>{" "}
         </section>
       </main>
-      <footer className="flex w-full items-center justify-center p-8 border-t">
+      <footer className="flex  w-full  items-center justify-center p-8 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">
           © 2024 e-park. Todos los derechos reservados.
         </p>
