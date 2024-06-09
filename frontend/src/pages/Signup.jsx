6bbbb8003e20247591ceb74e3027c891d05d552b
matias <xgalladex1234@gmail.com>
2024-06-08 20:12:09 -0400

    se agrega doble contraseña

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -131,6 +131,22 @@ export default function Signup() {
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
+              <div>
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-gray-500 dark:text-gray-400"
+                  htmlFor="password"
+                >
+                  Contraseña
+                </label>
+                <input
+                  className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 mt-1"
+                  id="password"
+                  placeholder="Ingresa tu contraseña"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
               <button
                 className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full"
                 type="submit"
